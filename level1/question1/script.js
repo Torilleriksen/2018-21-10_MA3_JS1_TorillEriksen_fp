458fd8f5496efdf933b464f26b5c2b377c3d47dd
Torilleriksen <36129734+Torilleriksen@users.noreply.github.com>
2018-10-21 20:12:08 +0300

    Update script.js

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,12 @@
 //Use RegEx to validate form
+function myFunction() {
+    document.getElementById("form").submit();
+}
+function validateEmail(email) {
+    var emailTrue = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailTrue.test(email);
+}
+function validatePhone(telephone) {
+    var phoneTrue = /^\d{10}$/;
+    return phoneTrue.test(telephone);
+}
